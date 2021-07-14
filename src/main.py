29780d7a568946a912656cd295f1bc3439c36dee
Yolann Sabaux <yolann.sabaux@gmail.com>
2021-07-14 21:09:14 +0200

    extract_text

--- src/main.py
+++ src/main.py
@@ -1,15 +1,65 @@
 import pytesseract
 from pdf2image import convert_from_path
-import glob
+from PIL import Image
+
+import os
+
+from rich.progress import track
+from rich.console import Console
+from rich.text import Text
 from rich.traceback import install
+
 install()
+console = Console()
 
-pdf = "dataset/testpdf.pdf"
+# Store all the pages of the PDF in a variable
+text = Text("Preprocessing pages ...")
+text.stylize("bold green")
+console.print(text)
 
-# images = convert_from_path(pdf)
+def extract_text(pdf):
+    pages = convert_from_path(pdf)
 
-# i = 1
-# for image in images:
-#     image_name = "dataset/Page_" + str(i) + ".jpg"  
-#     image.save(image_name, "JPEG")
-#     i = i+1
+    image_counter = 1
+
+    for page in track(pages, description="[cyan]Converting pages to images ..."):
+        page_name = "temp/page_" + str(image_counter) + ".jpg"  
+        page.save(page_name, "JPEG")
+        image_counter += 1
+
+    # Variable to get count of total number of pages
+    filelimit = image_counter-1
+    
+    # Creating a text file to write the output
+    outfile = "out_text.txt"
+    with open(outfile, "w") as f:
+        print("Empty output file created")
+
+
+    # Open the file in append mode so that 
+    # All contents of all images are added to the same file
+    with open(outfile, "a") as f:
+        # Iterate from 1 to total number of pages
+        for i in track(range(1, filelimit + 1), description='[cyan]Extracting text from images ...'):
+
+            filename = "temp/page_"+str(i)+".jpg"
+                
+            # Recognize the text as string in image using pytesserct
+            text = str(((pytesseract.image_to_string(Image.open(filename)))))
+        
+            text = text.replace('-\n', '') # Cleaning line-break/hyphen   
+        
+            # Finally, write the processed text to the file.
+            f.write(text)
+
+    files = os.listdir("temp/")
+
+    # deleting files from temp/
+    for file in track(files, description="[cyan]Deleting images from temp/ ..."):
+        os.remove(f'temp/{file}')
+
+    console.print("Everything is all set for categorising your file! :thumbs_up:") 
+
+pdf = "dataset/test_complicated.pdf"
+
+extract_text(pdf)
